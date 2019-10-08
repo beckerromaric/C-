8be455c7cc3dafd4f047e6270e62efc95b213e06
Becker R <becker.r@outlook.fr>
2019-10-08 10:46:31 +0200

    create

---
+++ ExerciceBouteille/ExerciceBouteille/Bottle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceBouteille
+{
+    class Bottle
+    {
+        private Boolean isOpen;
+        private float capacity;
+        private float currentVolume;
+
+        public Bottle(float _volume)
+        {
+            this.capacity = _volume;
+        }
+
+        public bool Open()
+        {
+            return this.isOpen = true;
+        }
+
+        public bool Close()
+        {
+            return isOpen = false;
+        }
+
+        public float Empty()
+        {
+            return capacity = 0.0f;
+        }
+
+ /*       public float Empty(float _volumeRetire)
+        {
+            
+        }
+
+        public float fill()
+        {
+
+        }
+
+        public float fill(float)
+        {
+
+        }
+
+        public float getCapacity()
+        {
+
+        }*/
+    }
+}
---
+++ ExerciceBouteille/ExerciceBouteille/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceBouteille
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bottle bottle = new Bottle(10.5f);
+
+            Console.WriteLine("La capacité de la bouteille est " + bottle.ToString());
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
